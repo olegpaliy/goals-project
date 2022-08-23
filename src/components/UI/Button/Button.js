47bbd758b06c67e3158e6c1095937b52c482884f
olegpaliy <goleh@ukr.net>
2022-08-23 19:17:35 +0300

    push

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,12 @@
+// import styled from "styled-components";
+import styles from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
